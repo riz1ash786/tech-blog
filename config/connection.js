e8027317d60ccb879b5bf1631dab144ea34ec163
Rizwan Ashraf <riz1ash786@gmail.com>
2022-03-30 22:02:56 +0100

    conn heroku and localhost setup

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,23 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+// import sequelize constructor
+const Sequelize = require("sequelize");
+
+require("dotenv").config();
 
 let sequelize;
 
+// deploy on Heroku
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+  // deploy on localhost
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
     }
   );
 }
