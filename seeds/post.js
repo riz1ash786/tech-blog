687d13e18d987c9c38f4fe2724c6404874875c0f
Rizwan Ashraf <riz1ash786@gmail.com>
2022-04-02 21:42:59 +0100

    formatting changes and styling

--- seeds/post.js
+++ seeds/post.js
@@ -3,12 +3,12 @@ const { Post } = require("../models");
 const postData = [
   {
     title: "That's unbelievable!",
-    content: "Is it really though? Really...is it unbelievable?",
+    content: "The truth was that it was very much believable",
     user_id: 1,
   },
   {
     title: "Who would have thought!?",
-    content: "I've been at it for a while now and hope it works",
+    content: "I've been at it for a while now and I just hope it works...",
     user_id: 2,
   },
   {
@@ -16,17 +16,18 @@ const postData = [
       "Sometimes with just a little patience you can achieve the greatest of things",
     content:
       "This is true but don't fool yourself in believing you achieved this without God's permission",
-    user_id: 2,
+    user_id: 3,
   },
   {
     title: "I enjoy all of my cars with a preference for the fast one",
-    content: "My brother has just picked up a brand new RSQ8 and I love it!",
+    content:
+      "My brother took delivery of his brand new RSQ8 yesterday and I love it!",
     user_id: 5,
   },
   {
     title: "I wonder what I will have for dinner tonight?",
     content:
-      "I'm trying ti concentrate but I've just received an email with a 25% discount code from Uber Eats",
+      "I'm trying to concentrate but I've just received an email with a discount code from Uber Eats",
     user_id: 4,
   },
 ];
