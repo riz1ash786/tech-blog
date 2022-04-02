b3dc930ac48dfc583685f7f06783180ee0619d0d
Rizwan Ashraf <riz1ash786@gmail.com>
2022-04-02 19:27:50 +0100

    date format altered

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,7 @@
 module.exports = {
-  format_time: (date) => {
-    return date.toLocaleTimeString();
-  },
   format_date: (date) => {
-    // Using JavaScript Date methods
-    return `${new Date(date).getMonth() + 1}/${new Date(
-      date
-    ).getDate()}/${new Date(date).getFullYear()}`;
+    return `${new Date(date).getDate()}/${
+      new Date(date).getMonth() + 1
+    }/${new Date(date).getFullYear()}`;
   },
 };
